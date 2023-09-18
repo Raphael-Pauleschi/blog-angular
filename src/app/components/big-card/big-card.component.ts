611f83099e89d5801dd243576d0d236d6182f1fe
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-09-18 14:13:41 -0300

    Inteligent big-card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
+    photoCover: string = "https://i.stack.imgur.com/y9DpT.jpg"
+    photoAlt: string = "placeholder"
+    cardTitle: string = "Saiu a nova versão do angular! CU"
+    cardDescription: String = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione similique tenetur eaque ad commodi! "
+
+    constructor(){}
 
 }

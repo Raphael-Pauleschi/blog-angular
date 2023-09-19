a73c70f91f557b4296e30676aa2bcdc9e29e09e3
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-09-19 14:23:51 -0300

    Fake data

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo": "https://scarletviolet.pokemon.com/_images/news/aug_08_23/p05_01.jpg",
+        "title":"Conheça Orgepon e seus sets",
+        "description":"Com suas máscaras que mudam sua tipagem e seu tera exclusivo, a orgepon aparenta ser uma excelente wallbreaker com seu poderoso stab, podendo também ser utilizada como uma suporte rápida já que tem acesso a knock-off, spikes e encore ",
+    },
+    {
+        "id":2,
+        "photo": "https://cdn.segmentnextimages.com/wp-content/uploads/2022/11/Pokemon-SV-Baxcalibur.jpg",
+        "title":"Baxcalibur foi banido de OU",
+        "descrition":"Com seu set de Dragon Dance e seus poderosos Glaive Rush e além de um bom bulk, já se era um mon dificil de lidar, mas com o seu novo move scale shot junto de loading dice e swords dance, aumentou ainda mais suas capacidades de sweeper",
+    },
+    {
+        "id":3,
+        "photo": "https://pokemongohub.net/wp-content/uploads/2022/07/ursa.jpg",
+        "title":"Ursaluna retorna para a National Dex",
+        "description":"Com o banimento de Margeana trouxe com sigo o declineo de trick room, foi comprovado que ursaluna não era tão problematica como inicialmente era",
+    },
+    {
+        "id":4,
+        "photo": "https://staticg.sportskeeda.com/editor/2022/09/e0527-16638082482130-1920.jpg",
+        "title":"Mesmo com o declínio da Stall, como Toxapex continua viável?",
+        "description":"Toxapex possui uma boa tipagem defensiva principalmente para as principais ameaças specials do meta, e graças a tais terem pouca defesa, o status baixo de special attacker o faz um excelente revenge killer, principalmente para iron valiant",
+    },
+    
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent {
-
+  @Input() photoCover: string =
+    'https://scarletviolet.pokemon.com/_images/news/aug_08_23/p05_01.jpg';
+  @Input() contentTitle: string = 'Meu artigo';
+  @Input() contentDescription: string = 'Opa mundo, vai bem? eu não';
 }
